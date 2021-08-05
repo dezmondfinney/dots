633e675cdb211517e55616110f10204cb04105ab
Dezmond Finney <dezmondf@gmail.com>
2021-08-05 11:03:58 -0400

    clean up; rm fn & replace w/ abbr; EDITOR = lvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,11 @@
 set -gx ALTERNATE_EDITOR ""
-set -gx EDITOR "nvim"
-set -gx PAGER "most"
+set -gx EDITOR lvim
+set -gx PAGER most
 set -gx PATH $PATH $HOME/bin $HOME/go/bin $HOME/.local/bin
 set -gx GOPATH $HOME/go
 set -gx INFOPATH /data/data/com.termux/files/usr/share/info/
 set -g fish_prompt_pwd_dir_length 9
-set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showdirtystate yes
 set __fish_git_prompt_color_branch magenta
 set __fish_git_prompt_char_dirtystate '!'
 set __fish_git_prompt_char_stagedstate '→'
@@ -14,47 +14,31 @@ set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
 
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
-    set SESSION_TYPE "remote/ssh"
+    set SESSION_TYPE remote/ssh
 end
 
 switch $USER
-    case "u0_a302"
+    case u0_a302
         set -gx USER_NAME "📱Note20"
-    case "u0_a235"
+    case u0_a235
         set -gx USER_NAME "📱s10"
     case "*"
         set -gx USER_NAME "💻 $USER@$hostname"
 end
 
-function fish_title;end
-
-function e
-  emacsclient --tty $argv
-end
-
-function battery
-    set bt_status (termux-battery-status | jq .status)
-    set bt_plug (termux-battery-status | jq .plugged)
-    set bt_percent (termux-battery-status | jq .percentage)
-    set bt_display $bt_percent"%"
-
-    echo $bt_display
-end
-
-function xx
-    exit
-end
-
-set __fish_prompt_showdirtystate 'yes'
+set __fish_prompt_showdirtystate yes
+set fish_greeting
 
 function fish_prompt
     echo "$USER_NAME" (prompt_pwd)(fish_git_prompt)
     echo "⁁ "
 end
 
-alias gs='git status'
+function fish_title
+end
 
-set fish_greeting
 abbr -a -g tx task export
 abbr -a -g ar ranger
 abbr -a -g ga git add
+abbr -a -g gs git status
+abbr -a -g xx exit
