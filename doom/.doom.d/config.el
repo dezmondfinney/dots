139c26658e933866134d289067f2ebe868dce0cc
Dezmond Finney <dezmondf@gmail.com>
2021-09-17 09:02:01 -0400

    fix org location for syncing

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -31,7 +31,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Sync/org/")
 ;; (setq org-journal-dir "~/Sync/org/journal/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
