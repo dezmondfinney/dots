e4e84e5352bb677d67802d7fa6cf709fbf77e4ab
Dezmond Finney <dezmondf@gmail.com>
2023-12-24 12:02:51 -0500

    update truezen with twilight and keybindings

--- nvim/.config/nvim/lua/plugins/truezen.lua
+++ nvim/.config/nvim/lua/plugins/truezen.lua
@@ -6,9 +6,16 @@ return {
 			-- your config goes here
 			-- or just leave it empty :)
       integrations = {
+          twilight = true,
           lualine = true
         }
 		 }
 	end,
-}
+  keys = {
+    { "<leader>Zn", "<cmd>TZNarrow<CR>", desc = "Zen Narrow" },
+    { "<leader>Zf", "<cmd>TZFocus<CR>", desc = "Zen Focus" },
+    { "<leader>Zm", "<cmd>TZMinimalist<CR>", desc = "Zen Minimal" },
+    { "<leader>Za", "<cmd>TZAtaraxis<CR>", desc = "Zen Full" },
+  }
+},
 }
