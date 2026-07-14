27127c72a01240dacba9b2d79f174a93ae75ff0f
Your Name <you@example.com>
2026-07-14 10:56:14 -0400

    feat(nvim): update obsidian configuration

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,23 +1,44 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*",
   lazy = true,
   ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  keys = {
+    { "<leader>os", "<cmd>Obsidian search<cr>", desc = "Obsidian Search" },
+    { "<leader>oq", "<cmd>Obsidian quick_switch<cr>", desc = "Obsidian Quick Switch" },
+    { "<leader>on", "<cmd>Obsidian new<cr>", desc = "Obsidian New Note" },
+    { "<leader>ot", "<cmd>Obsidian today<cr>", desc = "Obsidian Today" },
+    { "<leader>od", "<cmd>Obsidian dailies<cr>", desc = "Obsidian Dailies" },
+    { "<leader>ob", "<cmd>Obsidian backlinks<cr>", desc = "Obsidian Backlinks" },
+    { "<leader>ol", "<cmd>Obsidian links<cr>", desc = "Obsidian Links" },
+    { "<leader>or", "<cmd>Obsidian rename<cr>", desc = "Obsidian Rename" },
+    { "<leader>oi", "<cmd>Obsidian template<cr>", desc = "Obsidian Template" },
+    { "<leader>oe", "<cmd>Obsidian extract_note<cr>", mode = "v", desc = "Obsidian Extract Note" },
+    { "<leader>op", "<cmd>Obsidian paste_img<cr>", desc = "Obsidian Paste Image" },
+  },
   opts = {
+    legacy_commands = false,
+    picker = {
+      name = "snacks.picker",
+    },
     workspaces = {
       {
-        name = "BlackLands",
-        path = "~/Blacklands",
+        name = "Blacklands",
+        path = "/Users/dezmondfinney/Library/Mobile Documents/iCloud~md~obsidian/Documents/Blacklands",
       },
     },
-    -- Optional: nice-to-have settings
-    notes_subdir = "notes",
     daily_notes = {
-      folder = "dailies",
+      folder = "Journal",
       date_format = "%Y-%m-%d",
     },
+    templates = {
+      folder = "Templates",
+    },
+    note_id_func = function(title)
+      return require("obsidian.builtin").title_id(title)
+    end,
   },
 }
